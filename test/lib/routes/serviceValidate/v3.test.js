a3c1d7759323a3d29a02bf54929c0d1224b7be3b
James Sumners <james.sumners@gmail.com>
2018-01-01 14:00:04 -0500

    Fix merging of attributes from hooks

--- test/lib/routes/serviceValidate/v3.test.js
+++ test/lib/routes/serviceValidate/v3.test.js
@@ -102,3 +102,82 @@ test('returns valid xml with attributes', (t) => {
     t.is(memberOf[1].children[0].data, 'group2')
   })
 })
+
+test('merges data from multiple userAttributes hooks', (t) => {
+  t.plan(13)
+  const server = clone(serverProto)
+  server.validateService = async function (url) {
+    t.is(url, 'http://example.com')
+    return {name: 'foo', url}
+  }
+  server.validateST = async function (tid) {
+    t.is(tid, '123456')
+    return {tid}
+  }
+  server.invalidateST = async function (tid) {
+    t.is(tid, '123456')
+    return {tid, expired: false, valid: true}
+  }
+  server.getTGT = async function (tid) {
+    t.is(tid, '123456')
+    return {tid, userId: 'foo'}
+  }
+  server.trackService = async function () {}
+  server.jscasHooks = {
+    userAttributes: [
+      async function (id) {
+        return {
+          firstName: 'to_be_replaced',
+          email: 'foo@example.com',
+          memberOf: [
+            'group1',
+            'group2'
+          ]
+        }
+      },
+      async function (id) {
+        return {
+          firstName: 'Foo',
+          lastName: 'Bar',
+          memberOf: [
+            'group2',
+            'group3'
+          ]
+        }
+      }
+    ]
+  }
+  const req = {
+    log: nullLogger,
+    query: {
+      service: 'http://example.com',
+      ticket: '123456'
+    }
+  }
+  const reply = {
+    type (val) {
+      t.is(val, 'text/xml')
+    }
+  }
+  plugin(server, {}, async () => {
+    const xml = await server.p3serviceValidate(req, reply)
+    const $ = cheerio.load(xml)
+    const ele = $('cas\\:user')
+    t.is(ele.text(), 'foo')
+
+    const email = $('cas\\:email')
+    t.is(email.text(), 'foo@example.com')
+
+    const firstName = $('cas\\:firstName')
+    t.is(firstName.text(), 'Foo')
+
+    const lastName = $('cas\\:lastName')
+    t.is(lastName.text(), 'Bar')
+
+    const memberOf = $('cas\\:memberOf')
+    t.is(memberOf.length, 3)
+    t.is(memberOf[0].children[0].data, 'group1')
+    t.is(memberOf[1].children[0].data, 'group2')
+    t.is(memberOf[2].children[0].data, 'group3')
+  })
+})
